5713ce23345aa29e0a7d7155e3a2077efaccf352
otakustation <88060311+otakustation@users.noreply.github.com>
2021-08-05 13:15:39 -0300

    Update new.php

--- new.php
+++ new.php
@@ -1,5 +1,24 @@
-<?php
-    if (isset($_POST["acao"])){
-    echo "<script>alert('Formulário Foi enviado pelo método POST')</script>";
-    }
+<?php 
+
+if (isset($_POST["acao"])){ 
+
+   $nome=$_POST["nome"]; 
+   $telefone=$_POST["telefone"]; 
+   $email=$_POST["email"]; 
+   $radio=$_POST["novidades"]; 
+   $msg=$_POST["mensagem"]; 
+
+   echo "<p>Olá, ".$nome."</p>"; echo "<p>Seu email é: ".$email."</p>"; 
+
+   echo "<p>Seu telefone é: ".$telefone."</p>"; 
+
+   if ($radio=="sim"){ 
+      echo "<p>Você escolheu receber nossas novidades</p>"; 
+   }
+   elseif ($radio=="nao") { 
+      echo "<p>Você escolheu NÃO receber nossas novidades</p>"; 
+   } 
+   echo "<p>Sua mensagem é:<br/>".$msg."</p>"; 
+} 
+
 ?>
